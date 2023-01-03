69113a9c2a13f457e2745df49847acb77b9c750a
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2023-01-03 22:51:26 +0100

    add Class abteilungen

--- abteilungen/models.py
+++ abteilungen/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class abteilungen(models.Model):
+    titel = models.CharField(max_length=50)
+    content = models.TextField(max_length=10000)
+    
