48287176bbf06a4a943d33b3417cd1feebf84e60
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2023-01-03 23:18:25 +0100

    add image & author

--- abteilungen/models.py
+++ abteilungen/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -6,4 +7,9 @@ from django.db import models
 class abteilungen(models.Model):
     titel = models.CharField(max_length=50)
     content = models.TextField(max_length=10000)
-    
+    image = models.ImageField('abteilung1/')
+    author = models.ForeignKey(User, related_name='abteiung_author' ,on_delete=models.CASCADE )
+
+
+    def __str__(self):
+        return self.titel
