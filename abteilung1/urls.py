36ea0201a74c5c6da0a77e19b634e389675618b3
mahmoudbitar02 <bitarmahmoud909@gmail.com>
2023-01-04 21:19:53 +0100

    add Postlist class

--- abteilung1/urls.py
+++ abteilung1/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from abteilungen.views import Postlist
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', Postlist.as_view())
 ]
 
 
--- abteilungen/views.py
+++ abteilungen/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import abteilungen
 
 # Create your views here.
+
+
+from django.views.generic import ListView
+
+class Postlist(ListView):
+    model=abteilungen
